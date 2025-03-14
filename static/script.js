707ba84fac2dc601b4192ef9ff79a2f85b7febf2
faizan khan <127597797+FaizanCode102@users.noreply.github.com>
2025-03-14 13:07:33 +0530

    updated

--- static/script.js
+++ static/script.js
@@ -7,3 +7,18 @@ function openPopup(popupId) {
 function closePopup(popupId) {
   document.getElementById(popupId).style.display = 'none';
 }
+
+// hamburgermenu
+
+const hamburgermenu = document.querySelector("#hamburgermenu");
+const navMenu = document.querySelector("#navMenu");
+
+hamburgermenu.addEventListener("click", function () {
+    this.classList.toggle("active");
+    navMenu.classList.toggle("open");
+
+    // Update aria-expanded for accessibility
+    const isExpanded = this.classList.contains("active");
+    this.setAttribute("aria-expanded", isExpanded);
+});
+
